73a546e560ac886179ee42638e5b364ef6b37958
William Medrano <williammedrano57@gmail.com>
2022-11-17 22:36:09 -0600

    prueba

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './component/Main'
+import './component/style.css';
+
+function App() {
+  return (
+  <>
+  <Main/>
+  </>
+  );
+}
+
+export default App;
